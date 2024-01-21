03bad368adf6cb623a3d526bfc58280c7fcadef0
medaka <36759068+medaka0213@users.noreply.github.com>
2024-01-22 00:56:59 +0900

    daaemon.jsonの追加

--- scripts/add_insecure.sh
+++ scripts/add_insecure.sh
@@ -5,6 +5,7 @@ echo "INSECURE_REGISTRY: $1"
 DOCKER_CONFIG_FILE='/etc/docker/daemon.json'
 if [ -z "$1" ]; then
     echo "$1 is empty."
+    echo "{}" > "$DOCKER_CONFIG_FILE"
 else
     if [ ! -f "$DOCKER_CONFIG_FILE" ]; then
         # daemon.jsonが存在しない場合、新規にファイルを作成
@@ -14,5 +15,5 @@ else
         # jqコマンドを使用して、設定を追加（jqがインストールされていない場合はインストールが必要）
         sudo jq ". + {\"insecure-registries\": [\"$1\"]}" "$DOCKER_CONFIG_FILE" > tmp.json && mv tmp.json "$DOCKER_CONFIG_FILE"
     fi
-    chmod 777 "$DOCKER_CONFIG_FILE"
 fi
+chmod 777 "$DOCKER_CONFIG_FILE"
