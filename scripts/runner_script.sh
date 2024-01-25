1c72afab53adc426b57a51098d462b743ca6c994
medaka <36759068+medaka0213@users.noreply.github.com>
2024-01-25 11:23:42 +0900

    テスト環境を追加

--- scripts/runner_script.sh
+++ scripts/runner_script.sh
@@ -5,6 +5,7 @@ echo "Configuring runner..."
 echo "URL: $URL"
 echo "RUNNER_GROUP: $RUNNER_GROUP"
 echo "RUNNER_LABELS: $RUNNER_LABELS"
+echo "RUNNER_NAME: $RUNNER_NAME"
 
 # dockerの再起動
 echo "Starting docker..."
